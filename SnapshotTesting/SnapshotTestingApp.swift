5c614033b0e76ef3d1b349d4f76dd4583f80508f
Will Saults <wsaults@yahoo.com>
2022-12-17 13:01:24 -0600

    Create Xcode project

---
+++ SnapshotTesting/SnapshotTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotTestingApp.swift
+//  SnapshotTesting
+//
+//  Created by Will Saults on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
